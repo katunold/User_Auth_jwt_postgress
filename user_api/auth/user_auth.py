7ca7eaf787d91a3d5a34ec679eed591863039b4a
katunold <katunold94@gmail.com>
2018-09-19 12:39:57 +0300

    - Add functionality to cater for blacklisted tokens

--- user_api/auth/user_auth.py
+++ user_api/auth/user_auth.py
@@ -1,14 +1,16 @@
 """
 Authentication module for JWT token
 """
+import os
 
 import bcrypt
 import datetime
 
 import jwt
-from flask import current_app
 
+from user_api.models.token_model import Tokens
 from user_api.models.user_model import Users
+import user_api.run
 from user_api.utils.singleton import Singleton
 from user_api.models.user_model import UserModel
 
@@ -28,13 +30,13 @@ class Authenticate(metaclass=Singleton):
         """
         try:
             payload = {
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1),
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=5),
                 'iat': datetime.datetime.utcnow(),
                 'sub': user_id
             }
-            return jwt.encode(payload, current_app.config.get('SECRET_KEY'), algorithm='HS256')
+            val = jwt.encode(payload, user_api.run.APP.config.get('SECRET_KEY'), algorithm='HS256')
+            return val
         except Exception as ex:
-            print(ex)
             return ex
 
     @staticmethod
@@ -44,9 +46,14 @@ class Authenticate(metaclass=Singleton):
         :param auth_token:
         :return:
         """
+
         try:
-            payload = jwt.decode(auth_token, current_app.config.get('SECRET_KEY'))
-            return payload['sub']
+            payload = jwt.decode(auth_token, user_api.run.APP.config.get('SECRET_KEY'))
+            is_blacklisted_token = Tokens().check_blacklist(auth_token)
+            if is_blacklisted_token:
+                return 'Token blacklisted. Please log in again.'
+            else:
+                return payload['sub']
         except jwt.ExpiredSignatureError:
             return 'Signature expired. Please log in again.'
         except jwt.InvalidTokenError:
